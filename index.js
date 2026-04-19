e784e43dd3a6bf536034ae9502c50f6c10514941
unknown <ptrbilqiss@gmail.com>
2026-04-19 13:16:01 +0700

    ya

--- index.js
+++ index.js
@@ -1,26 +1,51 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'; 
 
-import bookRoutes from './routes/bookRoutes.js';
-import loanRoutes from './routes/loanRoutes.js';
-import memberRoutes from './routes/memberRoutes.js';
-import authorRoutes from './routes/authorRoutes.js';
-import categoryRoutes from './routes/categoryRoutes.js';
+import dotenv from 'dotenv'; 
 
-dotenv.config();
+import bookRoutes from './routes/bookRoutes.js'; 
 
-const app = express();
+import loanRoutes from './routes/loanRoutes.js'; 
 
-app.use(express.json());
+import memberRoutes from './routes/memberRoutes.js';  
 
-app.use('/api/books', bookRoutes);
-app.use('/api/loans', loanRoutes);
-app.use('/api/members', memberRoutes);
-app.use('/api/authors', authorRoutes);
-app.use('/api/categories', categoryRoutes);
+import authorRoutes from './routes/authorRoutes.js'; 
 
-app.get('/', (req, res) => {
-  res.send('Smart Library API is Running...');
-});
+import categoryRoutes from './routes/categoryRoutes.js'; 
 
-export default app;
+ 
+
+dotenv.config(); 
+
+ 
+
+const app = express(); 
+
+app.use(express.json()); 
+
+ 
+
+// Grouping Routes 
+
+app.use('/api/books', bookRoutes); 
+
+app.use('/api/loans', loanRoutes); 
+
+app.use('/api/members', memberRoutes); 
+
+app.use('/api/authors', authorRoutes); 
+
+app.use('/api/categories', categoryRoutes); 
+
+ 
+
+app.get('/', (req, res) => res.send('Smart Library API is Running...')); 
+
+ 
+
+const PORT = process.env.PORT || 3000; 
+
+app.listen(PORT, () => { 
+
+  console.log(`🚀 Server running on http://localhost:${PORT}`); 
+
+}); 
