62587b244e06a31d4b5200d89c4b94e8912e5843
unknown <ptrbilqiss@gmail.com>
2026-04-19 13:10:53 +0700

    hadah

--- index.js
+++ index.js
@@ -23,8 +23,4 @@ app.get('/', (req, res) => {
   res.send('Smart Library API is Running...');
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+export default app;
